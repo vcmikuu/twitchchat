cd278d1bc191ea77c69430b6fec06c826b6ae6f2
laynia <vc_mikuu@hotmail.com>
2024-08-01 17:54:57 +0200

    Slider Issues

--- include/config.hpp
+++ include/config.hpp
@@ -5,5 +5,5 @@
 DECLARE_CONFIG(ModConfig,
     CONFIG_VALUE(SendTwitchMessages, bool, "Send Twitch Messages", true, "Allow that the mod can use ur Twitch Token to send messages when someone requests an Song.");
     CONFIG_VALUE(TwitchToken, std::string, "Twitch Token", "INSERTHERE", "Required");
-    CONFIG_VALUE(MaxNPS, int, "Max Note Per Second", 2, "Change the NPS for requests.");
+    CONFIG_VALUE(MaxNPS, int, "Max Note Per Second", 0, 30, "Change the NPS for requests.");
 )
